25373f1872041b12681b941b6052b9b1e75f686b
candicefraisse1 <candice.fraisse2@gmail.com>
2024-06-18 15:25:41 +0200

    feature: test unitaires natifs

--- models/dim/dim_trips.sql
+++ models/dim/dim_trips.sql
@@ -7,7 +7,7 @@
 WITH join_trips_stop_times AS (
 SELECT SPLIT(a.trip_id, ':')[1] AS trip_code, CAST(SPLIT(SPLIT(a.trip_id, ':')[2], 'T')[1] AS date) as trip_date, a.route_id, a.direction_id, b.stop_id, b.stop_sequence, b.arrival_time, b.departure_time
 FROM {{ ref('stg_trips') }} a
-LEFT JOIN {{ source('dbt', 'stop_times') }} b
+LEFT JOIN {{ ref('stg_stop_times') }} b
 ON a.trip_id=b.trip_id)
 
 , temp_table AS (
